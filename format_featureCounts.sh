6441ea824007dd99f048131f9beb1717e96bdc6c
tagaoglu <73551859+tagaoglu@users.noreply.github.com>
2020-12-22 18:00:04 +0100

    Create format_featureCounts.sh

---
+++ format_featureCounts.sh
@@ -0,0 +1,4 @@
+# Script Name: format_featureCounts.sh
+
+tail -n+2 featureCounts.txt | cut -f1,7-22 > counts.txt
+sed -ie 's/\/data\/courses\/rnaseq\/toxoplasma_de\/mapping\///g' counts.txt; sed -ie 's/\.sorted\.bam//g' counts.txt
