3f6944ea53bdbe41d2dfe83d67b64145264b32fe
tagaoglu <73551859+tagaoglu@users.noreply.github.com>
2020-12-22 17:35:41 +0100

    Create samtools_view.sh

---
+++ samtools_view.sh
@@ -0,0 +1,37 @@
+Script Name: samtools_view.sh
+
+# Since this job was split between our group, this script was written just for 4 samples 
+# and sbatch command should be run like this: sbatch hisat2_mapping.sh SRR7821951 SRR7821952 SRR7821953 SRR7821969
+
+
+#!/bin/bash
+
+
+#SBATCH --mail-user=tugba.agaoglu@students.unibe.ch
+#SBATCH --mail-type=begin_,end
+#SBATCH --job-name=samtools_view
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=4000M
+#SBATCH --partition=pcourse80
+#SBATCH --output=/data/courses/rnaseq/toxoplasma_de/tagaoglu/messages_output/output_samtools_view_%j.o
+#SBATCH --error=/data/courses/rnaseq/toxoplasma_de/tagaoglu/messages_error/error_samtools_view_%j.e
+#SBATCH --array=0-3
+
+
+# Sample names passed as arguments. Do not introduce extensions in the sbatch command.
+SAMPLES=("$@")
+
+
+# Load modules 
+module add UHTS/Analysis/samtools/1.10
+
+
+# Create a working directory and go to the working directory
+mkdir /data/courses/rnaseq/toxoplasma_de/tagaoglu/2_map_reads/bam_files
+cd /data/courses/rnaseq/toxoplasma_de/tagaoglu/2_map_reads
+
+
+
+# Convert sam to bam with samtools
+samtools view -hbS hisat2_mapping/${SAMPLES[$SLURM_ARRAY_TASK_ID]}.sam > bam_files/${SAMPLES[$SLURM_ARRAY_TASK_ID]}.bam
