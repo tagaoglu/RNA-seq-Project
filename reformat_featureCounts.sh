e9caa0834a0f58b7d3b568d90848b3701e9360d1
tagaoglu <73551859+tagaoglu@users.noreply.github.com>
2020-12-24 21:19:39 +0100

    Update reformat_featureCounts.sh

--- reformat_featureCounts.sh
+++ reformat_featureCounts.sh
@@ -1,4 +1,6 @@
 # Script Name: reformat_featureCounts.sh
 
+#!/bin/bash
+
 tail -n+2 featureCounts.txt | cut -f1,7-22 > counts.txt
 sed -ie 's/\/data\/courses\/rnaseq\/toxoplasma_de\/mapping\///g' counts.txt; sed -ie 's/\.sorted\.bam//g' counts.txt
